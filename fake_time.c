e7a114c3ab39cc2b559d7c0eece1413026624a56
Maike Herzog <maike.herzog@gmx.de>
2026-06-15 15:39:16 +0200

    adapt library code to new simulation output with clknetsim

--- fake_time.c
+++ fake_time.c
@@ -9,18 +9,20 @@
 
 #define MAX_OFFSETS 10000
 
-double offsets[MAX_OFFSETS]; //gespeicherte Offsets
-int offset_count = 0; // Anzahl der gespeicherten Offsets
+double offsets[MAX_OFFSETS]; 
+int offset_count = 0; 
 static int initialized = 0; 
-time_t start_time = 0; //Referenzzeitpunkt
+time_t start_time = 0; 
 
-static __thread int in_init = 0; // Flag, um Rekursion zu verhindern
-static __thread int in_time = 0; // Flag, um Rekursion in time() zu verhindern
+static __thread int in_hook = 0; 
 
-static time_t (*real_time_func)(time_t *) = NULL;
+static int   (*real_clock_gettime)(clockid_t, struct timespec *) = NULL;
+static time_t (*real_time)(time_t *) = NULL;
+static int   (*real_adjtimex)(struct timex *) = NULL;
+static int   (*real_ntp_gettime)(struct ntptimeval *) = NULL;
 
-
-void load_offsets(){
+/* Column 2 from offset.log*/
+static void load_offsets(void){
 
     const char *path = getenv("SIMULATION_LOG_PATH");
     if (!path) {
@@ -35,206 +37,164 @@ void load_offsets(){
         return;
     }
 
-    char line[512];
-    
-    while (fgets(line, sizeof(line), file)) {
-
-        if (strstr(line, "combine offset")){
-            char *ptr = strstr(line, "offset");
-            if (ptr) {
-                ptr += strlen("offset");
-                
-                // to skip leading spaces
-                while (*ptr == ' ') ptr++;
-
-                double offset = strtod(ptr, NULL);
-
-                if (offset_count < MAX_OFFSETS) {
-                    offsets[offset_count++] = offset;
-                    //printf("Loaded offset: %f\n", offset);
-                } else {
-                    fprintf(stderr, "Max offsets reached\n");
-                    break;
-                }
-            }
-        }
+    double col1, col2;
+    while (offset_count < MAX_OFFSETS && fscanf(file, "%lf %lf", &col1, &col2) == 2) {
+        offsets[offset_count++] = col2;
     }
     fclose(file);
+    fprintf(stderr, "Loaded %d offsets from %s\n", offset_count, path);
 
-    printf("Loaded %d offsets\n", offset_count);
 }
 
-double get_offset(){
+static double get_offset(void){
     if (offset_count == 0) return 0.0;
 
-    time_t now = time(NULL);
-    printf("Current time: %ld\n", now);
-    printf("Start time: %ld\n", start_time);
-    //int index = (now - start_time) % offset_count; 
-    
-    // Für Interpolation 
-    double elapsed = difftime(now, start_time);
-    printf("Elapsed time: %f seconds\n", elapsed);
-    int index = (int)elapsed % offset_count;
-    printf("Using offset index: %d\n", index);
-    int next_index = (index + 1) % offset_count;
-    printf("Next offset index: %d\n", next_index);
+    struct timespec ts;
+    real_clock_gettime(CLOCK_REALTIME, &ts);
+    double elapsed = (double) (ts.tv_sec - start_time) + ts.tv_nsec * 1e9;
 
-    double frac = elapsed - (int)elapsed; // Bruchteil für Interpolation
+    int index = (int)elapsed;
+    if (index >= offset_count -1) return offsets[offset_count -1];
+    if (index < 0) return offsets[0];
 
-    double o1 = offsets[index];
-    double o2 = offsets[next_index];
+    double frac = elapsed - index; // for interpolation
+    return offsets[index] + frac * (offsets[index + 1] - offsets[index]);
 
-    return o1 + frac * (o2 - o1); // Lineare Interpolation
-
-    //return offsets[index];
 }
 
-void init_if_needed() {
-    if (initialized || in_init) return;
+static void init_if_needed(void) {
+    if (initialized) return;
 
-    in_init = 1;
+    initialized = 1;
+
+    real_clock_gettime = dlsym(RTLD_NEXT, "clock_gettime");
+    real_time = dlsym(RTLD_NEXT, "time");
+    real_adjtimex = dlsym(RTLD_NEXT, "adjtimex");
+    real_ntp_gettime = dlsym(RTLD_NEXT, "ntp_gettime");
 
     load_offsets();
 
-    if (!real_time_func) real_time_func = dlsym(RTLD_NEXT, "time");
-    start_time = real_time_func(NULL);
-    initialized = 1;
+    struct timespec ts;
+    real_clock_gettime(CLOCK_REALTIME, &ts);
+    start_time = ts.tv_sec;
 
-    in_init = 0;
 }
 
 /*
-Hook für time()
+Hook for time()
 
-Test mit LD_PRELOAD=./libfaketime.so date
+Test with LD_PRELOAD=./libfaketime.so date
 */
 time_t time(time_t *t) {
-    static int logged = 0;
-    if(in_time) return real_time_func(t); // Rekursion verhindern
-    in_time = 1;
-
-    if (!logged) {
-        printf("Fake time called\n");
-        logged = 1;
-    }
-    //fprintf(stderr, "Fake time called\n");
-    static time_t (*real_time)(time_t *) = NULL;
-
-    if (!real_time) {
-        real_time = dlsym(RTLD_NEXT, "time");
-    }
-
+    if(in_hook) return real_time(t); // stop recursion 
+    in_hook = 1;
     init_if_needed();
 
-    time_t real = real_time(NULL);
-
+    struct timespec ts;
+    real_clock_gettime(CLOCK_REALTIME, &ts);
     double offset = get_offset();
 
-    time_t fake = real + (time_t)offset;
+    time_t results = ts.tv_sec + (time_t)offset;
+    if (t) *t = results;
+    in_hook = 0;
+    return results;
 
-    if (t) *t = fake;
-    return fake;
 }
 
 /*
-Hook für clock_gettime()
+Hook for clock_gettime()
 
-Test mit LD_PRELOAD=./libfaketime.so ./test_clock_gettime
+Test with LD_PRELOAD=./libfaketime.so ./test_clock_gettime
 */
 int clock_gettime(clockid_t clk_id, struct timespec *tp) {
-    printf("Fake clock_gettime called\n");
 
-    static int (*real_clock_gettime)(clockid_t, struct timespec*) = NULL;
-    static time_t (*real_time_func)(time_t *) = NULL;
-
-    if (!real_clock_gettime)
-        real_clock_gettime = dlsym(RTLD_NEXT, "clock_gettime");
+    if (in_hook) return real_clock_gettime(clk_id, tp); // stop recursion
+    in_hook = 1;
+    init_if_needed();
 
     int ret = real_clock_gettime(clk_id, tp);
+    if (ret == 0){
+        double offset = get_offset();
+        long offset_sec = (long)offset;
+        long offset_nsec = (long)((offset - offset_sec) * 1e9);
 
-    init_if_needed();
-
-    double offset = get_offset();
-    //tp->tv_sec += (time_t)offset();
-    // um keine Nachkommastellen zu verlieren
-    tp->tv_sec += (long)((offset - (time_t)offset) * 1e9);
-    //tp->tv_sec += 3600; // hier ersetze ich das dann mit dem Offset
-
+        tp->tv_sec += offset_sec;
+        if (tp->tv_nsec >= 1000000000L){
+            tp->tv_sec += 1;
+            tp->tv_nsec -= 1000000000L;
+        }else if (tp->tv_nsec < 0){
+            tp->tv_sec -= 1;
+            tp->tv_nsec += 1000000000L;
+        }
+        tp->tv_nsec += offset_nsec;
+    }
+    in_hook = 0;
     return ret;
+
 }
 
 /*
-Hook für ntp_gettime()
+Hook for adjtimex()
 
-Test mit LD_PRELOAD=./libfaketime.so ./test_ntp_gettime
-*/
-int ntp_gettime(struct ntptimeval *ntv) {
-    printf("Fake ntp_gettime called\n");
-    static int (*real_ntp_gettime)(struct ntptimeval *) = NULL;
-
-    if (!real_ntp_gettime)
-        real_ntp_gettime = dlsym(RTLD_NEXT, "ntp_gettime");
-
-    int ret = real_ntp_gettime(ntv);
-
-    init_if_needed();
-
-    // Offset berechnen
-    double offset = get_offset();
-
-    // Zeit manipulieren
-    ntv->time.tv_sec += (time_t)offset;
-    ntv->time.tv_sec += (suseconds_t)((offset - (time_t)offset) * 1e9);
-
-    // Fehlerwerte setzen
-    ntv->maxerror = 500000; // 50ms
-    ntv->esterror = 1000000;  // 10ms
-
-    return ret;
-}
-
-/*
-Hook für adjtimex()
-
-Test mit LD_PRELOAD=./libfaketime.so ./test_adjtimex
+Test with LD_PRELOAD=./libfaketime.so ./test_adjtimex
 */
 int adjtimex(struct timex *tx) {
-    printf("Fake adjtimex called\n");
 
-    // Struktur zurücksetzen
-    memset(tx, 0, sizeof(struct timex)); 
-    
-    // Initialisierung 
+    if (in_hook) return real_adjtimex(tx); // stop recursion
+    in_hook = 1;
     init_if_needed();
 
+    int ret = real_adjtimex(tx);
     double offset = get_offset();
 
-    tx->modes = ADJ_OFFSET;
     tx->offset = (long)(offset * 1e6);
-    tx->freq = 0;
-    tx->status = 0;
+    in_hook = 0;
+    return ret;
 
-    return 0;
 }
 
 int ntp_adjtime(struct timex *tx) {
-    printf("Fake ntp_adjtime called\n");
 
-    // Struktur zurücksetzen
-    memset(tx, 0, sizeof(struct timex)); 
-
-    // Initialisierung 
+    if (in_hook) return real_adjtimex(tx); // stop recursion
+    in_hook = 1;
     init_if_needed();
 
+    int ret = real_adjtimex(tx);
     double offset = get_offset();
 
-    //tx->modes = ADJ_OFFSET;
-    tx->offset = (long)(offset * 1e6);
-    /*
-    tx->freq = 0;
-    tx->status = 0;
-    */
-    
-    return 0;
+    tx -> offset = (long)(offset * 1e6);
+    in_hook = 0;
+    return ret;
 }
+
+/*
+Hook for ntp_gettime()
+
+Test with LD_PRELOAD=./libfaketime.so ./test_ntp_gettime
+*/
+int ntp_gettime(struct ntptimeval *ntv) {
+
+    if (in_hook) return real_ntp_gettime(ntv); // stop recursion
+    in_hook = 1;
+    init_if_needed();
+
+    int ret = real_ntp_gettime(ntv);
+    if (ret >= 0) {
+        double offset = get_offset();
+        ntv->time.tv_sec += (long)offset;
+        ntv->time.tv_sec += (long)((offset - (long)offset) * 1e9);
+
+        if (ntv->time.tv_nsec >= 1000000L) {
+            ntv->time.tv_sec += 1;
+            ntv->time.tv_nsec -= 1000000L;
+        } else if (ntv->time.tv_nsec < 0) {
+            ntv->time.tv_sec -= 1;
+            ntv->time.tv_nsec += 1000000L;
+        }
+    }
+    in_hook = 0;
+    return ret;
+}
+
+
+
