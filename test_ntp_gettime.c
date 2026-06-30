e270d3c6c29b000e4fc5a45dfd30160d678fbbf5
Maike Herzog <maike.herzog@gmx.de>
2026-06-30 16:54:05 +0200

    adapt test ntp_gettime

--- test_ntp_gettime.c
+++ test_ntp_gettime.c
@@ -7,6 +7,7 @@ int main() {
     ntp_gettime(&ntv);
 
     printf("Zeit: %ld\n", ntv.time.tv_sec);
+    printf("Mikrosekunden: %ld\n", ntv.time.tv_usec);
     printf("Maxerror: %ld\n", ntv.maxerror);
     printf("Esterror: %ld\n", ntv.esterror);
 
