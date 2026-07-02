1d1af14f39e3a9e769b9369b7f74f1be2fd76eea
Maike Herzog <maike.herzog@gmx.de>
2026-07-02 15:02:14 +0200

    rename fields like in man page

--- test_adjtimex.c
+++ test_adjtimex.c
@@ -6,13 +6,13 @@ int main() {
 
     int ret = adjtimex(&tx);
 
-    printf("Return: %d\n", ret);
-    printf("Offset (us): %ld\n", tx.offset);
-    printf("Frequency: %ld\n", tx.freq);
-    printf("Status: %d\n", tx.status);
-    printf("Maxerror: %ld\n", tx.maxerror);
-    printf("Esterror: %ld\n", tx.esterror);
-    printf("Tolerance: %ld\n", tx.tolerance);
+    printf("return: %d\n", ret);
+    printf("offset (us): %ld\n", tx.offset);
+    printf("freq: %ld\n", tx.freq);
+    printf("maxerror: %ld\n", tx.maxerror);
+    printf("esterror: %ld\n", tx.esterror);
+    printf("status: %d\n", tx.status);
+    printf("constant: %ld\n", tx.constant);
 
     return 0;
 }
